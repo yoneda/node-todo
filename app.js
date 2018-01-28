0ed1c6f3226eff8232c2976aa98d2578383aeb24
shou <nsshowdream@gmail.com>
2018-01-29 01:28:09 +0900

    use nedb to add and remove tasks

--- app.js
+++ app.js
@@ -16,17 +16,35 @@ app.use(bodyParser.json());
 app.set("views",__dirname + "/views");
 app.set("view engine","ejs");
 
-// nedb のテスト
+// nedb の初期化
 var db = {}
-db.users = new NeDB({filename:"testfile"});
+db.users = new NeDB({filename:"taskfile"});
 db.users.loadDatabase();
-db.users.insert({name:"hoge"});
-db.users.insert({name:"fuga"});
-db.users.insert({name:"uga"});
 
 // トップ画面
 app.get("/",function(req,res){
-  res.render("index.ejs");
+  db.users.find({}, function (err, docs) {
+    res.render("index.ejs",{docs:docs});
+  });
+});
+
+app.post("/add",function(req,res){
+  var task = req.body.task;
+  var checked = 0;
+  db.users.insert({task:task,checked:checked});
+  db.users.find({}, function (err, docs) {
+    res.render("index.ejs",{docs:docs});
+  });
+});
+
+app.post("/delete",function(req,res){
+  var delete_id = req.body.delete_id;
+  db.users.remove({_id:delete_id},{},function(err,num){
+    console.log("%d task deleted!",num);
+  });
+  db.users.find({}, function (err, docs) {
+    res.render("index.ejs",{docs:docs});
+  });
 });
 
 app.listen(3000);
