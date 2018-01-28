bb80627447f7122cd8c7d3fa7b0960e6c7033cac
shou <nsshowdream@gmail.com>
2018-01-28 20:17:40 +0900

    npm install express, body-parser, ejs

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-var sys = require("sys");
-var http = require("http");
-var server = http.createServer(
-  function(request,response){
-    response.writeHead(200,{"Content-Type":"text/plain"});
-    response.write("helloworld");
-    response.end();
-  }
-).listen(3000);
+var express = require('express');
+var bodyParser = require("body-parser");
+var app = express();
 
-sys.log("server running at http://localhost:3000/");
+// 静的ファイル(image,css,javascript など)をpublicフォルダに格納するよ
+// publicフォルダにtest.pngがあった場合、"http://localhost:3000/images/test.png"でアクセスできるようになる
+app.use(express.static(__dirname+"/public"));
+
+// MEMO: 下記の※1と※2のコードはなくても動いた。本当に必要なのだろうか
+// urlencodedとjsonを初期化(※1)
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+// ejsを使う設定(※2)
+app.set("views",__dirname + "/views");
+app.set("view engine","ejs");
+
+// トップ画面
+app.get("/",function(req,res){
+  res.render("index.ejs");
+});
+
+app.listen(3000);
