06655272ad194fe62273542d1ddb348b7ee99ae9
shou <nsshowdream@gmail.com>
2018-01-28 20:36:06 +0900

    add nedb test

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 var express = require('express');
 var bodyParser = require("body-parser");
+var NeDB = require("nedb");
 var app = express();
 
-// 静的ファイル(image,css,javascript など)をpublicフォルダに格納するよ
-// publicフォルダにtest.pngがあった場合、"http://localhost:3000/images/test.png"でアクセスできるようになる
+// 静的ファイル(image,css,javascript) をpublic フォルダに格納。
+// test.pngにアクセスしたいときは、
+// http://localhost:3000/test.png
 app.use(express.static(__dirname+"/public"));
 
-// MEMO: 下記の※1と※2のコードはなくても動いた。本当に必要なのだろうか
-// urlencodedとjsonを初期化(※1)
+// bodyParser の初期化
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-// ejsを使う設定(※2)
+// ejs の初期化
 app.set("views",__dirname + "/views");
 app.set("view engine","ejs");
+
+// nedb のテスト
+var db = {}
+db.users = new NeDB({filename:"testfile"});
+db.users.loadDatabase();
+db.users.insert({name:"hoge"});
+db.users.insert({name:"fuga"});
+db.users.insert({name:"uga"});
 
 // トップ画面
 app.get("/",function(req,res){
